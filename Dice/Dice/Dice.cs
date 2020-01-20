d4a1cbd3e21821cdb720ee8b22c5de2bea2006ae
vladimirtischler <tischler.vladimir@gmail.com>
2020-01-20 09:47:25 +0100

    dfv

---
+++ Dice/Dice/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dice
+{
+    class Dice
+    {
+        public int CountOfWalls { get; set; }
+
+        public Dice(int countOfWalls)
+        {
+            CountOfWalls = countOfWalls; 
+        }
+
+        public int Throw()
+        {
+            Random number = new Random();
+            return number.Next(1, CountOfWalls);
+        }
+    }
+}
