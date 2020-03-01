99375889b2de0830e1fe75529b097b59a7406e1d
matejslovak <jogurtik1@gmail.com>
2020-03-01 21:58:36 +0100

    MVVM

---
+++ Dice/Dice/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Dice.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Dice.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();          
+            viewModel = new MainViewModel();
+            BindingContext = viewModel;
+            TextDice.Text = "Six edge dice";
+        }
+        private void WallsSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+            if (e.Value == true)
+            {
+                viewModel.SwitchOn();
+                TextDice.Text = "Ten edge dice";
+            }
+            else
+            {
+                viewModel.SwitchOff();
+                TextDice.Text = "Six edge dice";
+            }
+        }
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            DiceLabel.Text = viewModel.dice.Throw();
+        }
+    }
+}
